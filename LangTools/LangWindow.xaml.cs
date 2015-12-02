95ff544a4a4674449ba17fbe7687c0d620b5e975
wordtinker <wordtinker@yandex.ru>
2015-12-02 10:14:50 +0300

    Language edit window first draft

---
+++ LangTools/LangWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LangTools
+{
+    /// <summary>
+    /// Interaction logic for LangWindow.xaml
+    /// </summary>
+    public partial class LangWindow : Window
+    {
+        public LangWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
